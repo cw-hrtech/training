86b20cb5c0868eab2fc33c39136957710f24e768
mannv <anhmantk@gmail.com>
2020-02-10 06:28:56 +0700

    commit code

--- code/nuxtjs/pages/student/index.vue
+++ code/nuxtjs/pages/student/index.vue
@@ -5,16 +5,17 @@
       <NLink class="btn btn-outline-primary" :to="{name: 'index'}">Home Page</NLink>
       <NLink class="btn btn-outline-primary" :to="{name: 'student-create'}">Add new student</NLink>
     </div>
-    <table class="table table-striped">
+    <table v-if="students.length > 0" class="table table-striped">
       <thead>
       <tr>
-        <th scope="col">No</th>
-        <th scope="col">Code</th>
-        <th scope="col">Name</th>
-        <th scope="col">Age</th>
-        <th scope="col">Gender</th>
-        <th scope="col">Email</th>
-        <th scope="col">Address</th>
+        <th style="width: 50px">No</th>
+        <th>Code</th>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Gender</th>
+        <th>Email</th>
+        <th>Address</th>
+        <th></th>
       </tr>
       </thead>
       <tbody>
@@ -26,12 +27,17 @@
         <td>{{item.code}}</td>
         <td>{{item.name}}</td>
         <td>{{item.age}}</td>
-        <td>{{item.gender}}</td>
+        <td>{{item.gender == 1 ? 'Male' : 'Female'}}</td>
         <td>{{item.email}}</td>
         <td>{{item.address}}</td>
+        <td>
+          <NLink :to="{}">Edit</NLink>
+          <a>Delete</a>
+        </td>
       </tr>
       </tbody>
-
+    </table>
+    <div class="text-center">
       <button
         :class="{'disabled': currentPage <= 1}"
         class="btn btn-outline-info mr-3"
@@ -46,9 +52,7 @@
       >
         Next
       </button>
-
-      tmpPage: {{tmpPage}}
-    </table>
+    </div>
   </div>
 </template>
 
@@ -61,26 +65,41 @@
         currentPage: 1,
         totalPage: 1,
         perPage: 10,
-        tmpPage: 0
+        tmpPage: 0,
+        loadDataDone: false
       }
     },
-    async asyncData({$axios}) {
-      // let data = await $axios.$get('http://icanhazip.com')
-      let {data} = await $axios.$get('api/student')
+    async asyncData({$axios, route}) {
+      let page = route.query.page == undefined ? 1 : route.query.page
+      let {data} = await $axios.$get(`api/student?page=${page}`)
       return {
         students: data.data,
         currentPage: data.current_page,
         tmpPage: data.current_page,
         totalPage: data.last_page,
-        perPage: data.per_page
+        perPage: data.per_page,
+        loadDataDone: true
       }
     },
     methods: {
+      async loadData(page) {
+        this.loadDataDone = false
+        let {data} = await this.$axios.$get(`api/student?page=${this.tmpPage}`)
+
+        this.students = data.data
+        this.currentPage = data.current_page,
+          this.tmpPage = data.current_page
+        this.totalPage = data.last_page
+        this.perPage = data.per_page
+        this.loadDataDone = true
+        this.$router.push({name: 'student', query: { page: this.tmpPage }})
+      },
       nextPage() {
         if (this.currentPage >= this.totalPage) {
           return;
         }
         this.tmpPage++;
+        this.loadData(this.tmpPage);
       },
 
       prevPage() {
@@ -88,6 +107,7 @@
           return;
         }
         this.tmpPage--;
+        this.loadData(this.tmpPage);
       }
     }
 
